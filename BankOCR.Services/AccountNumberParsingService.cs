1770f75bbfff908f0f996a6e6a996e33f3223a5f
David.Goodwin <david.goodwin@fdmgroup.com>
2021-05-02 12:11:32 +0100

    Added comments to account number parsing service for rough implementation guide

--- BankOCR.Services/AccountNumberParsingService.cs
+++ BankOCR.Services/AccountNumberParsingService.cs
@@ -8,15 +8,18 @@ namespace BankOCR.Services
     {
         public string ParseOcrInput(string input)
         {
+            //Split the input in to each individual line of the OCR input and remove the first line as this is always blank so not needed for parsing
             var newLinesplits = input.Split("\r\n").ToList();
             newLinesplits.RemoveAll(x => string.IsNullOrEmpty(x));
 
+            //Take 3 chars from each line to create each individual digit as a separate string
             var ocrInputs = new List<string>();
             for (int i = 0; i < 27; i = i + 3)
             {
                 ocrInputs.Add(string.Concat(newLinesplits.Select(x => x.Substring(i, 3))));
             }
 
+            //Match each ocr input to a digit
             var ocrInputsAsDigits = new List<int>();
             foreach (var ocrInput in ocrInputs)
             {
@@ -77,6 +80,7 @@ namespace BankOCR.Services
                 }
             }
 
+            //Return the identified digits concatenated as a string
             return string.Join(string.Empty, ocrInputsAsDigits.Select(x => x));
         }
     }
