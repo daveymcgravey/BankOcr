e9ea697ee8ee42dea0d5c83eceed4335b590be79
David.Goodwin <david.goodwin@fdmgroup.com>
2021-05-02 13:00:24 +0100

    Added logic to make user story 2 unit tests pass

--- BankOCR.Services/AccountNumberValidationService.cs
+++ BankOCR.Services/AccountNumberValidationService.cs
@@ -1,4 +1,5 @@
 ﻿using BankOCR.Services.Interfaces;
+using System.Linq;
 
 namespace BankOCR.Services
 {
@@ -6,7 +7,17 @@ namespace BankOCR.Services
     {
         public bool IsChecksumValid(string accountNumber)
         {
-            throw new System.NotImplementedException();
+            var accountNumberAsDigits = accountNumber.ToCharArray().Select(x => char.GetNumericValue(x));
+
+            double checksumTotal = 0;
+            double multiplier = 1;
+            foreach (var number in accountNumberAsDigits.Reverse())
+            {
+                checksumTotal += number * multiplier;
+                multiplier++;
+            }
+
+            return checksumTotal % 11 == 0;
         }
     }
 }
