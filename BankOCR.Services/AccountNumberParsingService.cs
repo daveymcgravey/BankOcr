d2dca686f4dfb86c25646d8c3543bcd998e97c2a
David.Goodwin <david.goodwin@fdmgroup.com>
2021-05-02 12:03:52 +0100

    Updated logic in account number parsing service to correctly parse number for cases where digit is 0

--- BankOCR.Services/AccountNumberParsingService.cs
+++ BankOCR.Services/AccountNumberParsingService.cs
@@ -1,4 +1,5 @@
 ﻿using BankOCR.Services.Interfaces;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace BankOCR.Services
@@ -9,18 +10,29 @@ namespace BankOCR.Services
         {
             var newLinesplits = input.Split("\r\n").ToList();
             newLinesplits.RemoveAll(x => string.IsNullOrEmpty(x));
-            var digitAsOcr = string.Concat(newLinesplits.Select(x => x.Substring(0, 3)));
-            var zeroAsString = 
-                " _ " +
-                "| |" +
-                "|_|";
-
-            if (digitAsOcr.Equals(zeroAsString))
+            
+            var ocrInputs = new List<string>();
+            for (int i = 0; i < 27; i = i + 3)
             {
-                return "000000000";
+                ocrInputs.Add(string.Concat(newLinesplits.Select(x => x.Substring(i, 3))));
             }
 
-            return "";
+            var ocrInputsAsDigits = new List<int>();
+            foreach (var ocrInput in ocrInputs)
+            {
+                switch (ocrInput)
+                {
+                    case    " _ " +
+                            "| |" +
+                            "|_|":
+                        ocrInputsAsDigits.Add(0);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return string.Join(string.Empty, ocrInputsAsDigits.Select(x => x));
         }
     }
 }
