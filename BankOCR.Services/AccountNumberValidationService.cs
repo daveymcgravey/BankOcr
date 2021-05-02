460103c910ff6134a26c966c1bc0f35fff333265
David.Goodwin <david.goodwin@fdmgroup.com>
2021-05-02 13:02:13 +0100

    Added comments to account number validation service

--- BankOCR.Services/AccountNumberValidationService.cs
+++ BankOCR.Services/AccountNumberValidationService.cs
@@ -7,7 +7,18 @@ namespace BankOCR.Services
     {
         public bool IsChecksumValid(string accountNumber)
         {
+            //Parse account number to digits
             var accountNumberAsDigits = accountNumber.ToCharArray().Select(x => char.GetNumericValue(x));
+
+            //Loop through the account numbers (in reverse to make the maths easier) as per the following:
+
+            /*
+                account number:  3  4  5  8  8  2  8  6  5
+                position names:  d9 d8 d7 d6 d5 d4 d3 d2 d1
+
+                checksum calculation:
+                (d1+2*d2+3*d3+...+9*d9) mod 11 = 0
+             */
 
             double checksumTotal = 0;
             double multiplier = 1;
